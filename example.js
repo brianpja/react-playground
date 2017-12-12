b8e5e2512de3bc71926257cd2e694e455cdbde96
Brian James <brianpja@gmail.com>
2017-12-12 14:30:42 -0800

    object spread

--- example.js
+++ example.js
@@ -1,4 +1,26 @@
-console.log({
-  ...{hello: "World"}
-})
+// console.log({
+//   ...{hello: "World"}
+// })
 //test change
+
+const user = { username: 'angelica' }
+const adminUser = {
+  ...user,
+  admin: true
+}
+
+
+// console.log(user);
+// console.log(adminUser);
+
+const clock = {
+  hours: 13,
+  minutes: 40,
+  seconds: 23
+}
+
+const { seconds, ...clone } = clock;
+
+
+console.log(clock)
+console.log(clone)
